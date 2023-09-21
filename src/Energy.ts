133f5208af92836d4eab3427a6f50c412b163a9d
Thiesa <thiesa.c@gmail.com>
2023-09-21 12:18:46 -0400

    style: remoção de comentários desnecessários do cógido

--- src/Energy.ts
+++ src/Energy.ts
@@ -1,9 +1,3 @@
-// A interface deverá possuir os atributos:
-//  type_, do tipo EnergyType; ✨✨
-//    Esse novo tipo pode deve receber os valores: 'mana' ou 'stamina';
-//    O tipo EnergyType também deve ser exportado.
-//   amount, do tipo number
-// Para implementar a interface Energy, é necessário criar um tipo novo, o type EnergyType;
 type EnergyType = 'mana' | 'stamina';
 
 interface Energy {
